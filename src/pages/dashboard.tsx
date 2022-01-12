f146bfc183ccdf879f4d20d8617112f9cefd0062
LuizSouz404 <luiz.souz404@gmail.com>
2022-01-12 15:55:42 -0300

    Dev: Dealing with permissions and roles into some pages

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,20 +1,25 @@
 import { useContext } from "react"
 import { AuthContext } from "../contexts/AuthContext"
-import { setupAPIClient } from "../services/api";
+import { useCan } from "../hooks/useCan";
 import { withSSTAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const useCanSeeMetrics = useCan({
+    roles: ['editor', 'administrator']
+  })
+
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {useCanSeeMetrics && <h3>Métricas</h3>}
+    </>
   )
 }
 
 export const getServerSideProps = withSSTAuth(async (ctx) => {
-  const apiClient = setupAPIClient(ctx);
-  const response = await apiClient.get('/me');
-
   return {
     props: {}
   }
